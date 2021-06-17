d15f46f9b4d3882454314b935b37aacdffa27ba4
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-06-17 11:52:52 +0200

    [Frontend] Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import LocationsPage from "./pages/LocationsPage";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path={"/locations"} exact>
-            <LocationsPage/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path={"/locations"} exact>
+                    <LocationsPage/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import {AppBar, Toolbar} from "@material-ui/core";
+
+export default function Header() {
+
+    return (
+        <AppBar>
+            <Toolbar>
+                <p>
+                    PhotoHunter
+                </p>
+            </Toolbar>
+        </AppBar>
+    )
+}
