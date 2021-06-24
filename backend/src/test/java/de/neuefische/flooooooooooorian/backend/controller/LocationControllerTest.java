71d578c982d86734cc8005f773f696a51ecd5adf
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-06-24 16:47:58 +0200

    [Backend] Add LocationControllerTest

--- backend/src/test/java/de/neuefische/flooooooooooorian/backend/controller/LocationControllerTest.java
+++ backend/src/test/java/de/neuefische/flooooooooooorian/backend/controller/LocationControllerTest.java
@@ -1,22 +1,33 @@
 package de.neuefische.flooooooooooorian.backend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.flooooooooooorian.backend.dto.LocationCreationDto;
 import de.neuefische.flooooooooooorian.backend.model.Location;
 import de.neuefische.flooooooooooorian.backend.model.Picture;
 import de.neuefische.flooooooooooorian.backend.repository.LocationRepository;
 import de.neuefische.flooooooooooorian.backend.repository.PictureRepository;
+import de.neuefische.flooooooooooorian.backend.service.CloudinaryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LocationControllerTest {
@@ -32,6 +43,8 @@ class LocationControllerTest {
 
     @Autowired
     private PictureRepository pictureRepository;
+
+    private final CloudinaryService cloudinaryService = mock(CloudinaryService.class);
 
     @BeforeEach
     public void clearDb() {
@@ -120,5 +133,38 @@ class LocationControllerTest {
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), notNullValue());
         assertThat(response.getBody(), is(l2));
+    }
+
+    @Test
+    void createBasicLocationControllerIntegrationTest() throws IOException {
+        Picture picture = Picture.builder().url("testurl").id("fsfsdf").build();
+
+        when(cloudinaryService.uploadImage(Mockito.any(File.class))).thenReturn(picture);
+
+        LocationCreationDto dto = LocationCreationDto.builder()
+                .lat(50.0)
+                .lng(15)
+                .description("description l1")
+                .title("title")
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        MockMultipartFile imageFile = new MockMultipartFile("data", "other-file-name.data", "text/plain", "some other type".getBytes());
+        MockMultipartFile dtoFile = new MockMultipartFile("json", "", "application/json", mapper.writeValueAsString(dto).getBytes());
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", imageFile);
+        body.add("locationCreationDto", dtoFile);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<Location> response = testRestTemplate.postForEntity("http://localhost:" + port + "/api/location/", requestEntity, Location.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
     }
 }
