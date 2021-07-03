85c0d41e584c75d41c493ba0a0e9f08d656759b3
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-07-03 15:18:39 +0200

    [Backend] Fix GoogleLoginServiceTest

--- backend/src/test/java/de/neuefische/flooooooooooorian/backend/service/GoogleLoginServiceTest.java
+++ backend/src/test/java/de/neuefische/flooooooooooorian/backend/service/GoogleLoginServiceTest.java
@@ -28,21 +28,20 @@ class GoogleLoginServiceTest {
 
     @Test
     void loginWithGoogle() {
-        String code = "google_code";
+        String code = "code";
 
         when(googleLoginConfig.getClientId()).thenReturn("google_client_id");
         when(googleLoginConfig.getClientSecret()).thenReturn("google_client_secret");
         when(googleLoginConfig.getRedirectUri()).thenReturn("google_redirect_uri");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        HttpHeaders token_headers = new HttpHeaders();
+        token_headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.set("code", code);
         map.set("client_id", googleLoginConfig.getClientId());
         map.set("client_secret", googleLoginConfig.getClientSecret());
         map.set("grant_type", "authorization_code");
         map.set("redirect_uri", googleLoginConfig.getRedirectUri());
-        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, headers);
         GoogleAccessTokenDto googleAccessTokenDto = GoogleAccessTokenDto.builder()
                 .scope("scope")
                 .token_type("bearer")
@@ -50,13 +49,11 @@ class GoogleLoginServiceTest {
                 .expires_in("4684646")
                 .refresh_token("google_refresh_token")
                 .build();
-        ResponseEntity<GoogleAccessTokenDto> responseEntity = new ResponseEntity<>(googleAccessTokenDto, HttpStatus.OK);
-        when(restTemplate.exchange("https://oauth2.googleapis.com/token", HttpMethod.POST, httpEntity, GoogleAccessTokenDto.class)).thenReturn(responseEntity);
+        when(restTemplate.exchange("https://oauth2.googleapis.com/token", HttpMethod.POST, new HttpEntity<>(map, token_headers), GoogleAccessTokenDto.class))
+                .thenReturn(ResponseEntity.ok(googleAccessTokenDto));
 
-
-        headers = new HttpHeaders();
-        headers.setBearerAuth(googleAccessTokenDto.getAccess_token());
-        HttpEntity<String> entity = new HttpEntity<>(headers);
+        HttpHeaders userinfo_headers = new HttpHeaders();
+        userinfo_headers.setBearerAuth(googleAccessTokenDto.getAccess_token());
         GoogleProfileDto googleProfileDto = GoogleProfileDto.builder()
                 .verified_email(true)
                 .picture("picture_url")
@@ -66,7 +63,7 @@ class GoogleLoginServiceTest {
                 .email("testemail@gmail.com")
                 .build();
         ResponseEntity<GoogleProfileDto> profileResponseEntity = new ResponseEntity<>(googleProfileDto, HttpStatus.OK);
-        when(restTemplate.exchange("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", HttpMethod.GET, entity, GoogleProfileDto.class)).thenReturn(profileResponseEntity);
+        when(restTemplate.exchange("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", HttpMethod.GET, new HttpEntity<>(userinfo_headers), GoogleProfileDto.class)).thenReturn(profileResponseEntity);
 
         User user = User.builder()
                 .id("test_id")
@@ -84,11 +81,10 @@ class GoogleLoginServiceTest {
         claims.put("name", user.getFull_name());
         when(jwtUtilsService.createToken(claims, user.getEmail())).thenReturn("jwt_test_token");
 
+        String actual_token = googleLoginService.loginWithGoogle(code);
 
-        String actual_token = googleLoginService.loginWithGoogle("code");
-
-        verify(restTemplate).exchange("https://oauth2.googleapis.com/token", HttpMethod.POST, httpEntity, GoogleAccessTokenDto.class);
-        verify(restTemplate).exchange("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", HttpMethod.GET, entity, GoogleProfileDto.class);
+        verify(restTemplate).exchange("https://oauth2.googleapis.com/token", HttpMethod.POST, new HttpEntity<>(map, token_headers), GoogleAccessTokenDto.class);
+        verify(restTemplate).exchange("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", HttpMethod.GET, new HttpEntity<>(userinfo_headers), GoogleProfileDto.class);
         verify(userService).loginUserWithGoogle(googleProfileDto, googleAccessTokenDto);
         verify(jwtUtilsService).createToken(claims, user.getEmail());
 
