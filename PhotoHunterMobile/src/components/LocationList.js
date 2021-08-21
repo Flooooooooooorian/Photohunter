0180c47d57fbba1a454a1c220e7c62030d1817a0
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-08-21 22:59:16 +0200

    [Mobile] LocationList ScrollView

--- PhotoHunterMobile/src/components/LocationList.js
+++ PhotoHunterMobile/src/components/LocationList.js
@@ -1,25 +1,27 @@
 import LocationItem from "./LocationItem";
 import React from "react";
-import {Text, TouchableOpacity, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import Styles from "../Styles";
-import { Feather } from '@expo/vector-icons';
+import {Feather} from '@expo/vector-icons';
 
 export default function LocationList({locations, toggleView, showDetailsPage}) {
     const classes = Styles()
 
     return (
-        <>
-            <View style={classes.card}>
-                <View style={classes.space}/>
-                <Text style={classes.page_title}>
-                    Locations
-                </Text>
 
-                <TouchableOpacity onPress={toggleView}>
-                    <Feather name="map" size={24} color="black" />
-                </TouchableOpacity>
-            </View>
-            {locations.map((location) => <LocationItem key={location.id} showDetailsPage={showDetailsPage} location={location}/>)}
-        </>
+        <ScrollView style={classes.card}>
+            <View style={classes.space}/>
+            <Text style={classes.page_title}>
+                Locations
+            </Text>
+
+            <TouchableOpacity onPress={toggleView}>
+                <Feather name="map" size={24} color="black"/>
+            </TouchableOpacity>
+            {locations.map((location) => <LocationItem key={location.id} showDetailsPage={showDetailsPage}
+                                                       location={location}/>)}
+
+        </ScrollView>
+
     )
 }
