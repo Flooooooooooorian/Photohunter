77ef1dafb9b3b133613cdc65bcda9c3e4bb02712
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-08-23 19:29:32 +0200

    [Mobile] ServerConfig

--- PhotoHunterMobile/ServerConfig.js
+++ PhotoHunterMobile/ServerConfig.js
@@ -3,7 +3,7 @@
 export default function ServerConfig() {
     return (
         {
-            ip: "http://192.168.178.41:8080"
+            ip: "https://photohunter.herokuapp.com"
         }
     )
 }
